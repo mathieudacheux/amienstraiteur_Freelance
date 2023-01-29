cd13b53ba5961c257df4aac27f3ee1004a76e369
twinklose <mathieu.dacheux@icloud.com>
2023-01-29 10:20:10 +0100

    Connexion

---
+++ views/connexion.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-md-12 d-flex justify-content-center" style="height: 100vh;">
+        <div class="col-md-4 col-md-offset-4">
+            <form action="index.php" method="post">
+                <div class="form-group">
+                    <label for="login">Adresse-mail</label>
+                    <input type="text" class="form-control" id="login" name="login" placeholder="Login">
+                </div>
+                <div class="form-group my-2">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
+                </div>
+                <button type="submit" class="btn btn-primary">Connexion</button>
+            </form>
+        </div>
+    </div>
+</div>
